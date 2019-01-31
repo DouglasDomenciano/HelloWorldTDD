b83f90c9ca99bf788225708ed5c3bf5b86627e70
Douglas Domenciano <douglas.domenciano@bpfidelidade.com.br>
2019-01-31 15:41:06 -0200

    Preparing files to the publish in NPM

---
+++ build/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var helloWorld = function helloWorld() {
+  return 'Hello World';
+};
+var helloWorldWithName = function helloWorldWithName(name) {
+  return 'Hello World ' + name;
+};
+exports.helloWorld = helloWorld;
+exports.helloWorldWithName = helloWorldWithName;
